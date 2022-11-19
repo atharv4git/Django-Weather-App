99b357153e86d8b51d44c3cff204ed5703604c51
atharv4git <atharv4study@gmail.com>
2022-11-19 14:21:45 +0530

    c02

--- main/settings.py
+++ main/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/admin/css'
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATICFILES_DIRS=[
-os.path.join(BASE_DIR,'static')
+os.path.join(BASE_DIR,'admin/css')
 ]
 
